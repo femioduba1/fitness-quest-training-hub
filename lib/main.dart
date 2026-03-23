a151862cbf0f04d119d1edf60f4ced97872d11c0
Femi Oduba <femioduba@gmail.com>
2026-03-23 14:18:08 -0400

    add comments to main navigation and app structure for demo explanation

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'screens/exercise_library_screen.dart';
 import 'screens/create_quest_screen.dart';
 import 'screens/progress_screen.dart';
 
+// Entry point of the app
 void main() {
   runApp(const FitnessQuestApp());
 }
 
+// This is the root of the app
+// It sets up the overall theme and initial screen
 class FitnessQuestApp extends StatelessWidget {
   const FitnessQuestApp({super.key});
 
@@ -15,17 +18,25 @@ class FitnessQuestApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fitness Quest',
+
+      // Removes the debug banner in the top right
       debugShowCheckedModeBanner: false,
+
+      // Global app theme (colors, styling, etc.)
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
         scaffoldBackgroundColor: const Color(0xFFF6F8FB),
       ),
+
+      // Main navigation screen that controls all tabs
       home: const MainNavigation(),
     );
   }
 }
 
+// This widget controls the bottom navigation bar
+// and switches between different screens
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
 
@@ -34,8 +45,12 @@ class MainNavigation extends StatefulWidget {
 }
 
 class _MainNavigationState extends State<MainNavigation> {
+
+  // Keeps track of which tab is currently selected
   int _selectedIndex = 0;
 
+  // List of all screens in the app
+  // The index corresponds to the selected tab
   final List<Widget> _screens = const [
     HomeScreen(),
     ExerciseLibraryScreen(),
@@ -43,6 +58,8 @@ class _MainNavigationState extends State<MainNavigation> {
     ProgressScreen(),
   ];
 
+  // This function runs when a user taps a tab
+  // It updates the selected index and refreshes the UI
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -52,10 +69,16 @@ class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      // Displays the currently selected screen
       body: _screens[_selectedIndex],
+
+      // Bottom navigation bar for switching between screens
       bottomNavigationBar: NavigationBar(
         selectedIndex: _selectedIndex,
         onDestinationSelected: _onItemTapped,
+
+        // Each destination represents a screen/tab
         destinations: const [
           NavigationDestination(
             icon: Icon(Icons.home_outlined),
